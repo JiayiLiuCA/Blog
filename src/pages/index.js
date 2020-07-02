39d52ba74e47571f1e735aae81feb435621f9312
JiayiLiuCA <liujiayi402@gmail.com>
2020-07-02 15:12:39 -0400

    populate index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Navbar from '../components/Navbar'
 import Img from 'gatsby-image'
+import PostList from '../components/PostList'
 
 
 export default function IndexPage({ data }) {
@@ -11,17 +12,27 @@ export default function IndexPage({ data }) {
             <section className="hero has-gatsby-img">
                 <Img fluid={data.file.childImageSharp.fluid} />
                 <Navbar />
+                <div className="hero-body">Hello World</div>
                 <div className="hero-foot has-text-centered">
-                <svg viewBox="0 0 32 32" width="32" height="32">
-                    <title>scroll down</title>
-                    <path
-                        fill="#fff"
-                        d="M.045 8.443c0-.215.082-.43.246-.594.33-.33.86-.33 1.19 0L16 22.37 30.52 7.85c.33-.33.86-.33 1.19 0s.327.86 0 1.186L16.593 24.152c-.328.326-.86.326-1.188 0L.29 9.036c-.163-.163-.245-.378-.245-.593z"
-                    />
-                </svg>
-            </div>
+                    <svg viewBox="0 0 32 32" width="32" height="32">
+                        <title>scroll down</title>
+                        <path
+                            fill="#fff"
+                            d="M.045 8.443c0-.215.082-.43.246-.594.33-.33.86-.33 1.19 0L16 22.37 30.52 7.85c.33-.33.86-.33 1.19 0s.327.86 0 1.186L16.593 24.152c-.328.326-.86.326-1.188 0L.29 9.036c-.163-.163-.245-.378-.245-.593z"
+                        />
+                    </svg>
+                </div>
             </section>
-            <div>Hello World</div>
+            <section className="section">
+                <div className="container has-text-centered">
+                    <div className="content is-size-4">
+                        <p>
+                            Welcome to my blog!
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <PostList posts={data.latestPosts} />
         </Layout>
     )
 }
@@ -53,8 +64,8 @@ export const pageQuery = graphql`
                     frontmatter {
                         title
                         description
-                        templateKey
                         date(formatString: "MMMM DD, YYYY")
+                        tags
                     }
                 }
             }
