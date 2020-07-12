0e8e398e47549ff9e76bf5b0931c9b86930d71fe
JiayiLiuCA <liujiayi402@gmail.com>
2020-07-12 17:25:22 -0400

    fix Archive page refresh prop issue, fix error in gatsby build

--- src/pages/archives.js
+++ src/pages/archives.js
@@ -5,18 +5,19 @@ import Navbar from '../components/Navbar'
 import PostList from '../components/PostList'
 
 const ArchivesPage = ({ data: { posts }, location }) => {
-    //console.log(`render, location is ${location.state.searchText}`)
-    const [searchText, setSearchText] = useState(location.state.searchText || '')
-    //console.log(`searchText is ${searchText}`)
+    //console.log("location is " + location.state.searchText)
+    const [searchText, setSearchText] = useState(location.state ? location.state.searchText : '')
+    //console.log("searchText is " + searchText)
 
     //useEffect will check if location prop is changed, therefore no infinite loop
     //searchText will always be updated to location state when location changes
     //Changing searchText will not affect location state therefore useEffect() won't be called
 
     useEffect(() => { //location changes
-        //console.log("effect ")
-        if (location.state.searchText !== searchText) {
-            //console.log(location.state.searchText)
+        //console.log("effect : ")
+        //location state is undefined in building stage, add condition to avoid error
+        if (location.state && location.state.searchText !== searchText) { 
+            //console.log("setSearchState to " + location.state.searchText)
             setSearchText(location.state.searchText || '')
         }
     }, [location])
