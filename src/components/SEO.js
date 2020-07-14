8a94a9d9c3f5f8ab529e34afc893b2a65fc305d5
JiayiLiuCA <liujiayi402@gmail.com>
2020-07-13 21:42:19 -0400

    Add favicon to React Helmet

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import { withPrefix } from 'gatsby'
 
 const SEO = ({ title, description }) => {
     return (
@@ -7,6 +8,13 @@ const SEO = ({ title, description }) => {
             <html lang="en" />
             <title>{title}</title>
             <meta name="description" content={description} />
+
+            <link
+                rel='icon'
+                type='image/png'
+                sizes={'36x36'}
+                href={withPrefix(`/img/favicon/favicon-36.png`)}
+            />
 
             <meta property="og:site_name" content={title} />
             <meta property="og:url" content="/" />
